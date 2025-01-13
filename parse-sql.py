038dd2c94d287a87b361cd5a57be9af5994c8cd0
BitUniverse <64bituniverse@gmail.com>
2025-01-13 16:15:22 -0500

    Learning views

--- parse-sql.py
+++ parse-sql.py
@@ -1,176 +1,183 @@
+#
+# I THINK THIS FILE SUPERCEDED parseimap. USE THIS ONE.
+#
 import os
 import re
 import mariadb
 import sys
 import fnmatch
 
-def StartSQL():
-    global cur
-    try:
-        conn = mariadb.connect(
-            host="localhost",
-            user="pythonma",
-            password="password",
-            database="metroalerts"
-        )
-    except mariadb.Error as e:
-        print(f"Error connecting to MariaDB: {e}")
-        sys.exit(1)
-    conn.autocommit = False
-    cur = conn.cursor()
+class StartSQL():
+    def __init__(self):
+        super().__init__()
 
 
-def FileParser():
-    global alparts
-    global adparts
+        try:
+            self.conn = mariadb.connect(
+                host="localhost",
+                user="pythonma",
+                password="password",
+                database="metroalerts"
+            )
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB: {e}")
+            sys.exit(1)
+        self.conn.autocommit = False
+        self.cur = self.conn.cursor()
 
-    alparts = {}
-    adparts = {}
-    #Lists for alerts
-    alsub = []
-    aldat = []
-    albod = []
 
-    #Lists for advisories
-    adda = []
-    adho = []
-    adse = []
-    adma = []
-    adrl = []
-    adbl = []
-    adol = []
-    adsl = []
-    adyl = []
-    adgl = []
-    adet = []
+class FileParser():
+    def __init__(self):
+        super().__init__()
+    
 
-    # create a dictionary to store the filenames and their file types
-    files = []
+        self.alparts = {}
+        #adparts = {}
+        #Lists for alerts
+        self.alsub = []
+        self.aldat = []
+        self.albod = []
+        '''
+        #Lists for advisories
+        adda = []
+        adho = []
+        adse = []
+        adma = []
+        adrl = []
+        adbl = []
+        adol = []
+        adsl = []
+        adyl = []
+        adgl = []
+        adet = []
+        '''
+        # create a dictionary to store the filenames and their file types
+        self.files = []
 
-    # specify the folder containing the files
-    folder_path = './emails/'
+        # specify the folder containing the files
+        self.folder_path = './emails/'
 
-    # loop through all the files in the folder
-    for file_name in os.listdir(folder_path):
-        files.append(file_name)
+        # loop through all the files in the folder
+        for file_name in os.listdir(self.folder_path):
+            self.files.append(file_name)
 
-    # Open the file for reading
-    for i in files:
-        for file_name in os.listdir("./emails/"):
-            if fnmatch.fnmatch(file_name, "alerts-*.txt"):
-                with open("./emails/"+file_name, 'r') as f:
-                    # Read the contents of the file into a string variable
-                    file_contents = f.read()
-                #Get the certain string from the txt file
-                sub = re.search('Subject: (.+?)Date: ',file_contents)
-                date = re.search("Date: (.+?)"+str(r'-0700'), file_contents)
-                body = re.search(str(r'-0700')+" (.+?)OptOut: ", file_contents)
-                if sub:
-                    sub = sub.group(1)
-                    alsub.append(sub)
-                    
-                if date:
-                    date = date.group(1)
-                    aldat.append(date)
+        # Open the file for reading
+        for i in self.files:
+            for file_name in os.listdir("./emails/"):
+                if fnmatch.fnmatch(file_name, "alerts-*.txt"):
+                    with open("./emails/"+file_name, 'r') as f:
+                        # Read the contents of the file into a string variable
+                        self.file_contents = f.read()
+                    #Get the certain string from the txt file
+                    self.sub = re.search('Subject: (.+?)Date: ',self.file_contents)
+                    self.date = re.search("Date: (.+?)"+str(r'-0700'), self.file_contents)
+                    self.body = re.search(str(r'-0700')+" (.+?)OptOut: ", self.file_contents)
+                    if self.sub:
+                        self.sub = self.sub.group(1)
+                        self.alsub.append(self.sub)
+                        
+                    if self.date:
+                        self.date = self.date.group(1)
+                        self.aldat.append(self.date)
 
-                if body:
-                    body = body.group(1)
-                    albod.append(body)
+                    if self.body:
+                        self.body = self.body.group(1)
+                        self.albod.append(self.body)
+                '''
+                else:
+                    with open("./emails/"+file_name, 'r') as f:
+                        file_contents = f.read()
+                    da = re.search('(.+?)' + str(r'\nHours:'),file_contents)
+                    ho = re.search(str(r'\nHours:') + '(.+?)' + str(r'\nService:'),file_contents)
+                    se = re.search(str(r'\nService:') + '(.+?)' + str(r'\nMaintenance:'),file_contents)
+                    ma = re.search(str(r'\nMaintenance:')+'(.+?)' + str(r'\nRed Line'),file_contents)
+                    rl = re.search(str(r'\nRed Line') + '(.+?)' + str(r'\nBlue Line'),file_contents)
+                    bl = re.search(str(r'\nBlue Line') + '(.+?)' + str(r'\nOrange Line'),file_contents)
+                    ol = re.search(str(r'\nOrange Line') + '(.+?)' + str(r'\nSilver Line'),file_contents)
+                    sl = re.search(str(r'\nSilver Line') + '(.+?)' + str(r'\nYellow Line'),file_contents)
+                    yl = re.search(str(r'\nYellow Line') + '(.+?)' + str(r'\nGreen Line'),file_contents)
+                    gl = re.search(str(r'\nGreen Line') + '(.+?)' + str(r'\nFor more information'),file_contents)
+                    et = re.search(str(r'\nFor more information') + '(.+?)',file_contents)
 
-            else:
-                with open("./emails/"+file_name, 'r') as f:
-                    file_contents = f.read()
-                da = re.search('(.+?)' + str(r'\nHours:'),file_contents)
-                ho = re.search(str(r'\nHours:') + '(.+?)' + str(r'\nService:'),file_contents)
-                se = re.search(str(r'\nService:') + '(.+?)' + str(r'\nMaintenance:'),file_contents)
-                ma = re.search(str(r'\nMaintenance:')+'(.+?)' + str(r'\nRed Line'),file_contents)
-                rl = re.search(str(r'\nRed Line') + '(.+?)' + str(r'\nBlue Line'),file_contents)
-                bl = re.search(str(r'\nBlue Line') + '(.+?)' + str(r'\nOrange Line'),file_contents)
-                ol = re.search(str(r'\nOrange Line') + '(.+?)' + str(r'\nSilver Line'),file_contents)
-                sl = re.search(str(r'\nSilver Line') + '(.+?)' + str(r'\nYellow Line'),file_contents)
-                yl = re.search(str(r'\nYellow Line') + '(.+?)' + str(r'\nGreen Line'),file_contents)
-                gl = re.search(str(r'\nGreen Line') + '(.+?)' + str(r'\nFor more information'),file_contents)
-                et = re.search(str(r'\nFor more information') + '(.+?)',file_contents)
+                    if da:
+                        da = da.group(1)
+                        adda.append(da)
 
-                if da:
-                    da = da.group(1)
-                    adda.append(da)
+                    if ho:
+                        ho = ho.group(1)
+                        adho.append(ho)
 
-                if ho:
-                    ho = ho.group(1)
-                    adho.append(ho)
+                    if se:
+                        se = se.group(1)
+                        adse.append(se)
 
-                if se:
-                    se = se.group(1)
-                    adse.append(se)
+                    if ma:
+                        ma = ma.group(1)
+                        adma.append(ma)
 
-                if ma:
-                    ma = ma.group(1)
-                    adma.append(ma)
+                    if rl:
+                        rl= rl.group(1)
+                        adrl.append(rl)
 
-                if rl:
-                    rl= rl.group(1)
-                    adrl.append(rl)
+                    if bl:
+                        bl = bl.group(1)
+                        adbl.append(bl)
 
-                if bl:
-                    bl = bl.group(1)
-                    adbl.append(bl)
+                    if ol:
+                        ol = ol.group(1)
+                        adol.append(ol)
 
-                if ol:
-                    ol = ol.group(1)
-                    adol.append(ol)
+                    if sl:
+                        sl = sl.group(1)
+                        adsl.append(sl)
 
-                if sl:
-                    sl = sl.group(1)
-                    adsl.append(sl)
+                    if yl:
+                        yl = yl.group(1)
+                        adyl.append(yl)
 
-                if yl:
-                    yl = yl.group(1)
-                    adyl.append(yl)
+                    if gl:
+                        gl = gl.group(1)
+                        adgl.append(gl)
 
-                if gl:
-                    gl = gl.group(1)
-                    adgl.append(gl)
-
-                if et:
-                    et = et.group(1)
-                    adet.append(et)
-
-    #Alert Dictionary
-    #Convert list to tuple
-    tasu = tuple(alsub)
-    tada = tuple(aldat)
-    tabo = tuple(albod)
-    #Add tuples to dictionary
-    alparts["Subject:"] = tasu
-    alparts["Date:"] = tada
-    alparts["Body:"] = tabo
-
-    #Advisory Dictionary
-    tadda = tuple(adda)
-    print(tadda)
-    tadho = tuple(adho)
-    tadse = tuple(adse)
-    tadma = tuple(adma)
-    tadrl = tuple(adrl)
-    tadbl = tuple(adbl)
-    tadol = tuple(adol)
-    tadsl = tuple(adsl)
-    tadyl = tuple(adyl)
-    tadgl = tuple(adgl)
-    tadet = tuple(adet)
-    adparts["Date:"] = tadda
-    adparts["Hours:"] = tadho
-    adparts["Service:"] = tadse
-    adparts["Maintenance:"] = tadma
-    adparts["Red Line:"] = tadrl
-    adparts["Blue Line:"] = tadbl
-    adparts["Orange Line:"] = tadol
-    adparts["Silver Line:"] = tadsl
-    adparts["Yellow Line:"] = tadyl
-    adparts["Green Line:"] = tadgl
-    adparts["Etcetera:"] = tadet
-
+                    if et:
+                        et = et.group(1)
+                        adet.append(et)
+                    '''
+        #Alert Dictionary
+        #Convert list to tuple
+        self.tasu = tuple(self.alsub)
+        self.tada = tuple(self.aldat)
+        self.tabo = tuple(self.albod)
+        #Add tuples to dictionary
+        self.alparts["Subject:"] = self.tasu
+        self.alparts["Date:"] = self.tada
+        self.alparts["Body:"] = self.tabo
+        '''
+        #Advisory Dictionary
+        tadda = tuple(adda)
+        print(tadda)
+        tadho = tuple(adho)
+        tadse = tuple(adse)
+        tadma = tuple(adma)
+        tadrl = tuple(adrl)
+        tadbl = tuple(adbl)
+        tadol = tuple(adol)
+        tadsl = tuple(adsl)
+        tadyl = tuple(adyl)
+        tadgl = tuple(adgl)
+        tadet = tuple(adet)
+        adparts["Date:"] = tadda
+        adparts["Hours:"] = tadho
+        adparts["Service:"] = tadse
+        adparts["Maintenance:"] = tadma
+        adparts["Red Line:"] = tadrl
+        adparts["Blue Line:"] = tadbl
+        adparts["Orange Line:"] = tadol
+        adparts["Silver Line:"] = tadsl
+        adparts["Yellow Line:"] = tadyl
+        adparts["Green Line:"] = tadgl
+        adparts["Etcetera:"] = tadet
+        '''
 if __name__ == "__main__":
     StartSQL()
     FileParser()
--- wcscmetro/main/views.py
+++ wcscmetro/main/views.py
@@ -1,9 +1,82 @@
+import os
+import re
+import mariadb
+import sys
+import fnmatch
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Alerts, Advisories
 
 # Create your views here.
+class Home:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def sqlStart(self):
+        try:
+            conn = mariadb.connect(
+                host="localhost",
+                user="pythonma",
+                password="password",
+                database="metroalerts"
+            )
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB: {e}")
+            sys.exit(1)
+        conn.autocommit = False
+        self.cur = conn.cursor()
+
+    def parser(self,request):
+        self.alparts = {}
+        #adparts = {}
+        #Lists for alerts
+        self.alsub = []
+        self.aldat = []
+        self.albod = []
+
+        self.files = []
+
+        # specify the folder containing the files
+        self.folder_path = './emails/'
+
+        # loop through all the files in the folder
+        for file_name in os.listdir(self.folder_path):
+            self.files.append(file_name)
+
+        # Open the file for reading
+        for i in self.files:
+            for file_name in os.listdir("./emails/"):
+                if fnmatch.fnmatch(file_name, "alerts-*.txt"):
+                    with open("./emails/"+file_name, 'r') as f:
+                        # Read the contents of the file into a string variable
+                        self.file_contents = f.read()
+                    #Get the certain string from the txt file
+                    self.sub = re.search('Subject: (.+?)Date: ',self.file_contents)
+                    self.date = re.search("Date: (.+?)"+str(r'-0700'), self.file_contents)
+                    self.body = re.search(str(r'-0700')+" (.+?)OptOut: ", self.file_contents)
+                    if self.sub:
+                        self.sub = self.sub.group(1)
+                        self.alsub.append(self.sub)
+                        
+                    if self.date:
+                        self.date = self.date.group(1)
+                        self.aldat.append(self.date)
+
+                    if self.body:
+                        self.body = self.body.group(1)
+                        self.albod.append(self.body)
+
+        #Alert Dictionary
+        #Convert list to tuple
+        self.tasu = tuple(self.alsub)
+        self.tada = tuple(self.aldat)
+        self.tabo = tuple(self.albod)
+        #Add tuples to dictionary
+        self.alparts["Subject:"] = self.tasu
+        self.alparts["Date:"] = self.tada
+        self.alparts["Body:"] = self.tabo
 def main(request):
     return render(request, 'index.html', {
         "alerts_list": Alerts.objects.all(),
