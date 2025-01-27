fd0b91d35f3616e3e14b0781261f1fc802543148
64bituniverse <64bituniverse@gmail.com>
2025-01-26 20:33:02 -0500

    Working email header remover. Now need to figure out BS4 or something else.

--- enumtest.py
+++ enumtest.py
@@ -1,8 +1,10 @@
+import linecache
+import email
+import email.policy
 import re
 import quopri
 import os
 import json
-from bs4 import BeautifulSoup
 from tempfile import TemporaryFile
 from email.iterators import _structure
 
@@ -21,20 +23,23 @@ with open("emails/temp-format", "w+b") as temp:
     with open(maildir + "202406052347.1eb89e6d95214abf8ba9c2907ffaa3bc-NVZWS5D4IFBVGRKNIFEUYLKQKJHUILSDGJDDAOJXHE3UKRJSGE2EKQ2CHFCDMNRYGQ3TGMSEGRBDCQ.eml", "rb") as f:
         quopri.decode(f, temp)
     f.close()
+
     temp.seek(0)
+
     array = temp.readlines()
+
     for index, value in enumerate(array):
         if re.search("MIME-Version: 1.0", str(value)):
             j = index
+            print(value)
+            print(index)
     print(j)
 temp.close()
+
 del array[0:j + 2]
+
 with open("emails/temp-html", "w+b") as temp:
     for k in array:
+        print(k)
         temp.write(k)
-
-    temp.seek(0)
-    soup = BeautifulSoup(temp.read(), "lxml")
-
-    img = soup.find_all("img")
-    print(img)
+temp.close()
